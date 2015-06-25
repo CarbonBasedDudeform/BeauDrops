6e0d415fbd89a630ab9bd54e40ac7da82123ae98
Dom Musgrave <imdomdude@gmail.com>
2015-06-25 21:46:33 +0100

    Looks like cartoon water
    yup, sure does

--- Scripts/droplet.js
+++ Scripts/droplet.js
@@ -4,10 +4,10 @@ var Droplet = function(x,y, width, height)
   this.Y = y;
   this.Width = width;
   this.Height = height;
-  this.Colour = "rgb(100%, 100%, 100%)";
-  this.Red = 25 * Math.random();
-  this.Green = 50 * Math.random();
-  this.Blue = 100 * Math.random();
+  this.Colour = "rgb(0%, 0%, 0%)";
+  this.Red = 0;
+  this.Green = 0;
+  this.Blue = 0;
   this.KineticEnergy = 0;
   this.RedWeight = 1;
   this.GreenWeight = 1;
@@ -16,20 +16,6 @@ var Droplet = function(x,y, width, height)
   //red, green, blue as percentages
   this.SetColour = function(red, green, blue) {
     this.Colour = "rgb("+red +"%," + green + "%," + blue + "%)";
-  }
-
-  this.Update = function () {
-
-
-    var r = this.Red;// * this.KineticEnergy;// * this.RedWeight;
-    var g = this.Green;// * this.KineticEnergy;// * this.GreenWeight;
-    var b = this.Blue;// * this.BlueWeight;
-
-
-    this.SetColour(r,g,b);
-
-    //if(this.KineticEnergy > 1) this.KineticEnergy = 0.9;
-    //if (this.KineticEnergy < 0.1) this.KineticEnergy = 0.1;
   }
 
   this.Render = function(context) {
@@ -42,14 +28,7 @@ var Droplet = function(x,y, width, height)
     context.globalAlpha = 1;
   }
 
-  this.ResetColour = function (){
-    this.Red = 25 * Math.random();
-    this.Green = 50 * Math.random();
-    this.Blue = 100 * Math.random();
-  }
-
   this.Stimulate = function(energy) {
     this.KineticEnergy = energy;
-    ResetColour();
   }
 };
--- Scripts/index.js
+++ Scripts/index.js
@@ -2,9 +2,9 @@ var Grid = [];
 var context;
 var canvasElement;
 var height = 100;
-var width = 50;
-var dropletX = 0.5;
-var dropletY = 0.5;
+var width = 100;
+var dropletX = 1;
+var dropletY = 1;
 
 function onLoad() {
   canvasElement = document.getElementById("canvas");
@@ -16,12 +16,8 @@ function onLoad() {
     }
   }
 
-  Grid.forEach(function(element) {
-    element.Update();
-  });
-
   window.setInterval("GameLoop()", 30);
-  window.setInterval("randomlyStimulate()", 300);
+  window.setInterval("randomlyStimulate()", 15);
 }
 
 function GameLoop() {
@@ -31,20 +27,15 @@ function GameLoop() {
 
 var length=(width*height);
 function Update() {
+
   for(var i = 0; i < length; ++i) {
     var sum = 0;
-    var r = 0;
-    var g = 0;
-    var b = 0;
 
     function applyLaw(gridCell) {
       if (typeof(gridCell) != 'undefined')
       {
         sum += gridCell.KineticEnergy;
-        gridCell.KineticEnergy *= 0.996;
-        r += gridCell.Red;
-        g += gridCell.Green;
-        b += gridCell.Blue;
+        gridCell.KineticEnergy *= 0.998;
       }
     }
 
@@ -57,40 +48,27 @@ function Update() {
     applyLaw(Grid[i+width+1]);
     applyLaw(Grid[i-width+1]);
 
-    if (Grid[i].KineticEnergy <= (sum/8))
-    {
-      //console.log(sum / 8);
-      Grid[i].KineticEnergy = sum / 8;
+    if (Grid[i].KineticEnergy < sum/8){
+      Grid[i].KineticEnergy = sum/8;
     }
-
-    if (Grid[i].Red > r/8) {
-      //Grid[i].Red = r/8;
-    }
-
-    if (Grid[i].Green > g/8)
-    {
-      //Grid[i].Green = g/8;
-    }
-
-    if (Grid[i].Blue > 0 )
-    {
-      //Grid[i].Blue = b/8;
-    }
-
-    Grid[i].Update();
   }
 }
 
 function Render() {
+  context.clearRect(0,0,height,width);
+
   Grid.forEach(function (element) {
-    element.Render(context);
+    if (element.KineticEnergy > 0.001)  element.Render(context);
   });
+
+  context.globalAlpha = 0.1;
+  context.fillStyle = "rgb(25%, 65%,75%)";
+  context.fillRect(0,0, height, width);
 }
 
 function randomlyStimulate()
 {
   var randomIndex = Math.floor(Math.random() * Grid.length);
   var epicentre = Grid[randomIndex];
-  epicentre.ResetColour();
-  epicentre.Stimulate(1.75);
+  epicentre.Stimulate(1);
 }
