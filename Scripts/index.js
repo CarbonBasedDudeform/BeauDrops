9639a5abcf4157f3a5ae3ba4a8f7ade7ad7ec851
Dom Musgrave <imdomdude@gmail.com>
2015-06-27 13:35:52 +0100

    Some Kind of Blue
    Some Kind of Commit

--- Scripts/index.js
+++ Scripts/index.js
@@ -35,7 +35,7 @@ function Update() {
       if (typeof(gridCell) != 'undefined')
       {
         sum += gridCell.KineticEnergy;
-        gridCell.KineticEnergy *= 0.998;
+        gridCell.KineticEnergy *= 0.996;
       }
     }
 
@@ -58,7 +58,7 @@ function Render() {
   context.clearRect(0,0,height,width);
 
   Grid.forEach(function (element) {
-    if (element.KineticEnergy > 0.001)  element.Render(context);
+    if (element.KineticEnergy > 0.1)  element.Render(context);
   });
 
   context.globalAlpha = 0.1;
